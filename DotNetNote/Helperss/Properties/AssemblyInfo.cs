ec1bcea922f0a2f385a713866b4f7c412d6ec28d
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-04-14 17:25:05 +0900

    0414 커밋

---
+++ DotNetNote/Helperss/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helperss")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helperss")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97315ac8-d25b-4076-bc1c-24c63dc40b58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNetNote/Helperss/StringLibraryTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Helpers.Test
+{
+    [TestClass]
+    public class StringLibraryTest
+    {
+        [TestMethod]
+        public void CutStringTest()
+        {
+            string strCut = "Hello World, This is a test sentence";
+            int intChar = 15;
+
+            var expected = "Hello World,...";//
+            var actual = StringLibrary.CutString(strCut, intChar);
+            Assert.AreEqual(expected, actual);
+
+            
+        }
+
+        [TestMethod]
+        public void CutStringUnicodeTest()
+        {
+            string strcut = "안녕하세요. 부경대학교입니다.";
+            int intchar = 9;
+
+            var expected = "안녕하세요....";
+            var actual = StringLibrary.CutStringUnicode(strcut, intchar);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
